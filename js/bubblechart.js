4228f88d621210de617c08e7e508dadd4264e000
christopherjarch <archuleta@wisc.edu>
2020-03-24 00:45:59 -0500

    Bubblechart complete

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -50,7 +50,7 @@ window.onload = function(){
     // which is scaled according to the range and domain defined below
     // This is a generator since it interpolates new values
     var x = d3.scaleLinear()
-      .range([90, 810])
+      .range([90, 750])
       .domain([0, 3])
     var dataArray = [10, 20, 30, 40, 50];
 
@@ -73,6 +73,7 @@ window.onload = function(){
       }
     ];
 
+
     // get the minimum value of the array for scaling on the y-axis
     var minPop = d3.min(cityPop, function(d){
       return d.population;
@@ -83,11 +84,24 @@ window.onload = function(){
     });
 
     var y = d3.scaleLinear()
-    .range([440, 95])
+    .range([450, 50])
     .domain([
-      minPop,
-      maxPop
+      0,
+      700000
     ]);
+
+    // D3 can interpolate color values too
+    var color = d3.scaleLinear()
+      // The total range of colors falls between these two values
+      // An unclassed color scheme results
+      .range([
+        "#fdbe85",
+        "#d94701"
+      ])
+      .domain([
+        minPop,
+        maxPop
+      ]);
 
     // All elements called circles are selected here
     // selectAll is used to create different circles simultaneously
@@ -102,7 +116,7 @@ window.onload = function(){
       .attr("id", function(d){
         return d.city;
       })
-      // Below are defining the attributes of the circles with anonymous functions
+      // Below is defining the attributes of the circles with anonymous functions
       .attr("r", function(d, i){
         var area = d.population * 0.01;
         return Math.sqrt(area/Math.PI);
@@ -113,5 +127,65 @@ window.onload = function(){
       .attr("cy", function(d){
         return y(d.population);
       })
+      // The fill is based on the population
+      .style("fill", function(d, i){
+        return color(d.population);
+      })
+      .style("stroke", "#000");
+
+    // Create a y axis with a generator function
+    var yAxis = d3.axisLeft(y);
+
+    // Append attributes to the y axis
+    var axis = container.append("g")
+      .attr("class", "axis")
+      // Translate the axis 50 pixels to the right to make it visible
+      .attr("transform", "translate(50, 0)")
+      .call(yAxis);
+
+    // Block to make text element
+    var title = container.append("text")
+      .attr("class", "title")
+      .attr("text-anchor", "middle")
+      .attr("x", 450)
+      .attr("y", 30)
+      .text("City Populations");
+
+    // Join cityPop to the following conatiner to label the circles by city
+    var labels = container.selectAll(".labels")
+      .data(cityPop)
+      .enter()
+      .append("text")
+      .attr("class", "labels")
+      .attr("text-anchor", "left")
+      .attr("y", function(d){
+        return y(d.population) - 5;
+      });
+
+    // Break down bubble labels to two lines
+    // The tspan element is a child of a text element
+    var nameLine = labels.append("tspan")
+      .attr("class", "nameLine")
+      .attr("x", function(d,i){
+        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+      })
+      .text(function(d){
+        return d.city;
+      });
+
+    // Adds commas to the population labels
+    var format = d3.format(",");
+
+    // Second line of label, with the position
+    // established relative to the nameLine
+    var popLine = labels.append("tspan")
+      .attr("class", "popLine")
+      .attr("x", function(d,i){
+        return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+      })
+      .attr("dy", "15")
+      .text(function(d){
+        return "Pop. " + format(d.population);
+      });
 };
 // // Scripts by Chris Archuleta, 2020
