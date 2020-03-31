81f6349e1e1471059c5cf82dc77613934405d0e0
christopherjarch <archuleta@wisc.edu>
2020-03-31 04:18:28 -0500

    Activity 9 Lesson 1 Completed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = setMap();
 function setMap(){
   // Promise.all waits for the last dataset to load to callback all of the datasets
   var promises = [d3.csv("data/VoteTurn_Eric_Duffin_Statista_2_3 3_17_CDC_3_24.csv"),
-                  d3.json("data/StateCovid19Vote.json")
+                  d3.json("data/StateCovid19Vote.topojson")
                   // The above methods are AJAX methods
                 ];
   Promise.all(promises).then(callback);
@@ -21,6 +21,6 @@ function setMap(){
     // Parameters: TopoJSON feature, object with dataset's details
     var statesGeoJson = topojson.feature(states, states.objects.StateCovid19Vote);
 
-    console.log(statesGeoJSON);
+    console.log(statesGeoJson);
   };
 };
